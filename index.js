4f9dd21fb641c9ae9cb69c49b60ff30588a8a395
Chris Henry <cdhenry@gmail.com>
2018-04-24 20:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
-
-  function loadIssue() {
-    var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-    for(var i=0;i<issues.length;i++) {
-      var result = template(issues[i]);
-      document.getElementsByTagName("main")[0].innerHTML += result;
-    }
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
   }
 }
